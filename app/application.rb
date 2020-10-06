4a2a823cb7b2227389ca6e1a039b29f5a8a38dc7
Christie Yoon <cleex1999@gmail.com>
2020-10-06 01:15:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,10 @@
 class Application
 
   def call(env)
+    return [ 200, {'Content-Type' => 'text/html'}, has_key ]
+  end
+
+  def call(env)
     (Time.now.hour < 12)? ["Good Morning!"] : ["Good Afternoon!"]
   end
 end
